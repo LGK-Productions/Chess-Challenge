fdbbace997ae4da9d76b5fba7a536087dbae69a6
Apates <43420982+Apates@users.noreply.github.com>
2023-07-25 09:38:10 +0200

    Added pruning to first layer

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -16,8 +16,11 @@ public class MyBot : IChessBot
 
     private int msPerTurn = 100;
     private float lastMoveFinishedTime;
+    private int branchesSearched;
     public Move Think(Board board, Timer timer)
     {
+        branchesSearched = 0;
+        
         //adjust target time for move calculation if necessary
         if (timer.MillisecondsRemaining < msPerTurn * 300)
         {
@@ -29,53 +32,68 @@ public class MyBot : IChessBot
         bool isWhite = board.IsWhiteToMove;
         int bestValue = isWhite ? int.MinValue : int.MaxValue;
         Move bestMove = new Move();
+        int alpha = int.MinValue;
+        int beta = int.MaxValue;
         foreach (Move move in board.GetLegalMoves())
         {
             lastMoveFinishedTime = timer.MillisecondsElapsedThisTurn;
             board.MakeMove(move);
-            int eval = MinMax(board, timer, 6, int.MinValue, int.MaxValue);
-            
-            if (isWhite ? eval > bestValue : eval < bestValue)
+            int eval = MinMax(board, timer, 6, alpha, beta);
+
+            if ((isWhite ? eval > bestValue : eval < bestValue))
             {
                 bestValue = eval;
                 bestMove = move;
             }
             board.UndoMove(move);
+
+            if (isWhite)
+            {
+                alpha = Math.Max(alpha, bestValue);
+                if (beta <= alpha)
+                    break;
+            } else 
+            {
+                beta = Math.Min(beta, bestValue);
+                if (beta <= alpha)
+                    break;
+            }
         }
 
+        Console.WriteLine("Searched " + branchesSearched + " branches. Best Move is " + bestMove.MovePieceType + " to " + bestMove.TargetSquare + ". Eval: " + bestValue);
         return bestMove;
     }
 
-    public int MinMax(Board board, Timer timer, int depth, int alpha, int beta)
+    private int MinMax(Board board, Timer timer, int depth, int alpha, int beta)
     {
         if (depth <= 0)
             return Eval(board);
         
         bool isWhite = board.IsWhiteToMove;
-        int bestValue = isWhite ? Int32.MinValue : Int32.MaxValue;
+        int bestValue = isWhite ? int.MinValue : int.MaxValue;
+
         foreach (var move in board.GetLegalMoves())
         {
-            int nextDepth =
-                depth - Math.Max(1, (int)((timer.MillisecondsElapsedThisTurn - lastMoveFinishedTime) / msPerTurn * depth)); 
+            int nextDepth = depth - Math.Max(1, (int)((timer.MillisecondsElapsedThisTurn - lastMoveFinishedTime) / msPerTurn * depth));
+                
             board.MakeMove(move);
             if (isWhite)
+                bestValue = isWhite ? Math.Max(bestValue, MinMax(board, timer, nextDepth, alpha, beta)) : Math.Min(bestValue, MinMax(board, timer, nextDepth, alpha, beta));
+            board.UndoMove(move);
+
+            if (isWhite)
             {
-                bestValue = Math.Max(bestValue, MinMax(board, timer, nextDepth, alpha, beta));
-                board.UndoMove(move);
-                if (bestValue > beta)
-                    break;
                 alpha = Math.Max(alpha, bestValue);
-            }
-            else
+                if (beta <= alpha)
+                    break; 
+            } else
             {
-                bestValue = Math.Min(bestValue, MinMax(board, timer, nextDepth, alpha, beta));
-                board.UndoMove(move);
-                if (bestValue < alpha)
+                beta = Math.Min(beta, bestValue);
+                if (beta <= alpha)
                     break;
-                beta = Math.Min(alpha, bestValue);
             }
-            
         }
+
         return bestValue;
     }
 
@@ -91,6 +109,8 @@ public class MyBot : IChessBot
         {
             eval += (board.GetPieceList(pieceType, true).Count - board.GetPieceList(pieceType, false).Count) * (int)pieceValues[pieceType];
         }
+
+        branchesSearched++;
         return eval;
     }
 }
